8a485c1213f93db8ad3a60e4726f9797f2578e57
Fran Borg <francesca.bte@outlook.com>
2019-09-23 16:23:45 +0200

    Add movie class

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,24 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import '../styles/main.scss';
+import 'babel-polyfill';
 // Import any additional modules you want to include below \/
+import {Client} from "../scripts/apiCall.js"
 
+class Movies {
+    constructor() {
+        this.movies = ["amsterdam", "paris"]
+        this.client = new Client();
+    }
 
+    displayMovie() {
+        this.client.getMovieData("love and other drugs").then(data => {
+            console.log(data);
+        })
+    }
+
+}
+
+let movies = new Movies();
+movies.displayMovie();
 // \/ All of your javascript should go here \/
 
